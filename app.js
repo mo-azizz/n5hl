3d0cb4ef24d51ff02dace2ca82aa7c46d2751c75
N05h3ll <m.ashm985@gmail.com>
2020-06-23 02:04:52 +0200

    Final edits, TODO testing and debugging

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 require ('dotenv').config();
 const randomString = require('randomstring');
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
+app.use(helmet());
+app.use(express.static('public'));
+const fs = require('fs');
 const monk = require('monk');
 const db = monk(process.env.MONGO_URI)
 const bodyParser = require('body-parser');
 const yup = require('yup');
+const morgan = require('morgan');
+const path = require('path');
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+app.use(morgan('combined', { stream: accessLogStream }))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -13,34 +21,44 @@ let shortcuts= db.get('shortcuts');
 shortcuts.createIndex({extension:1},{unique: true});
 let schema = yup.object().shape({
     url: yup.string().trim().url().required(),
-    extension: yup.string().trim().matches(/[\w]/),
-})
+    extension: yup.string().trim().matches( /^[a-zA-Z0-9]$/),
+});
+let error;
+let complete;
 app.get('/', (req,res) => {
-
-    res.render('index');
+    
+    res.render('index', {complete,error});
     
 });
 app.post('/addNewUrl', async(req,res) =>{
     //TODO PROCESS POST REQUEST TO ADD NEW URL.
+    try{
     let { url, extension } = req.body;
     if (!extension){extension = randomString.generate(5)}else{
         let existing = await shortcuts.findOne({extension});
         if (existing){
-            res.send("ALREADY EXIST!!");
+            
+            throw new Error(`the extension "${extension}"  ALREADY EXISTS!!`);
+
         }
     }
     await schema.validate({
         url,
         extension,
     });
+
     extension = extension.toLowerCase();
     const newShortCut = {
         url,
         extension
     };
     let complete = await shortcuts.insert(newShortCut);
-    console.log(complete);
-    res.send(complete)
+    res.render('index', {complete : complete.extension,error});
+}catch (error){
+    console.log(error)
+    if (error.toString().includes('match')) error = 'ValidationError: extension must only contain alphabetical characters a-z A-Z or numbers 0-9';
+ res.render('index', {complete,error})
+}
 });
 app.get('/all', async (req,res) => {
     let result = await shortcuts.find({});
@@ -51,8 +69,15 @@ app.get('/:extension', async (req,res) => {
     let extension = req.params.extension;
     let existing = await shortcuts.findOne({extension});
     if (existing){res.redirect(existing.url)}else{
-        res.send('NOT FOUND !!');
+        error = 'ALREADY EXISTING'
     }
 });
-
+app.use((error, req, res, next) => {
+    if (error.status) {
+      res.status(error.status);
+    } else {
+      res.status(500);
+    }
+    res.render('index',{complete,error});
+  });
 app.listen(process.env.PORT, () => console.log('app listinig on 127.0.0.1'));
